c9cfb9703af225abc3a11d905ff99faf8a4ae2fc
Yaroslav Marchenko <yarik_marik_00@mail.ru>
2021-02-06 23:50:30 +0300

    add some filtering methods to CarRepository

--- src/main/java/org/yarik/testTask/repository/CarRepository.java
+++ src/main/java/org/yarik/testTask/repository/CarRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import org.yarik.testTask.model.Car;
 import org.yarik.testTask.model.CarBrand;
 import org.yarik.testTask.model.CarModel;
+import org.yarik.testTask.model.TypeAuto;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +15,13 @@ public interface CarRepository extends JpaRepository<Car, Long> {
     List<Car> getAll();
     List<Car> findAllByCarBrand(CarBrand carBrand);
     List<Car> findAllByCarBrandAndCarModel(CarBrand carBrand, CarModel carModel);
+    List<Car> findAllByCarBrandOrderByPrice(CarBrand carBrand);
+    List<Car> findAllByPriceBetween(int minPrice, int maxPrice);
+    List<Car> findAllByYearBetween(int minYear, int maxYear);
+    List<Car> findAllByMileageBefore(int maxMileage);
+    List<Car> findAllByVolumeOfEngineBetween(double minValue, double maxValue);
+    List<Car> findAllByDeleteIs(boolean isDelete);
+    List<Car> findAllByTypeAuto(TypeAuto typeAuto);
     Optional<Car> findById(long id);
     Car save(Car car);
 }
