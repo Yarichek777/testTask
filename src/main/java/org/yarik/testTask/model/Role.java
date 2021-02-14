bd4cd962b9a95345531672cda274ea1bee8b8039
Yaroslav Marchenko <yarik_marik_00@mail.ru>
2021-02-14 13:01:59 +0300

    add UserDetailsImplements and @JsonIgnore into User

--- src/main/java/org/yarik/testTask/model/Role.java
+++ src/main/java/org/yarik/testTask/model/Role.java
@@ -1,5 +1,12 @@
 package org.yarik.testTask.model;
 
-public enum Role {
-    ADMIN,USER
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    USER, ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
--- src/main/java/org/yarik/testTask/model/User.java
+++ src/main/java/org/yarik/testTask/model/User.java
@@ -1,18 +1,22 @@
 package org.yarik.testTask.model;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.Collection;
 import java.util.Set;
 
 @Entity
 @Data
+@EqualsAndHashCode(of = {"id"})
 @NoArgsConstructor
 @Table(name = "usr")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
@@ -20,8 +24,16 @@ public class User {
     @Size(min = 3, max = 20)
     private String login;
     @NotBlank(message = "field 'password' must not be empty!")
-    @Size(min = 3, max = 20)
+    @Size(min = 3)
+    @Column(length = 1024)
     private String password;
+    @OneToMany(
+            mappedBy = "user",
+            fetch = FetchType.EAGER,
+            orphanRemoval = true
+    )
+    @JsonIgnore
+    private Set<Car> cars;
     @Enumerated(EnumType.STRING)
     @ElementCollection(
             targetClass = Role.class,
@@ -31,4 +43,41 @@ public class User {
     private Set<Role> roles;
     @NotBlank(message = "field 'isActive' must not be empty!")
     private boolean isActive;
+
+    @JsonIgnore
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @JsonIgnore
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
+
 }
