c778db08d2e1fb8628b384d79c3b5547ba551c57
Yaroslav Marchenko <yarik_marik_00@mail.ru>
2021-02-15 20:52:25 +0300

    add configurations for JWT

--- src/main/java/org/yarik/testTask/config/WebSecurityConfig.java
+++ src/main/java/org/yarik/testTask/config/WebSecurityConfig.java
@@ -3,11 +3,16 @@ package org.yarik.testTask.config;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.yarik.testTask.jwt.JwtConfigurer;
+import org.yarik.testTask.jwt.JwtTokenProvider;
 import org.yarik.testTask.service.UserService;
 
 @Configuration
@@ -15,33 +20,41 @@ import org.yarik.testTask.service.UserService;
 @AllArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserService userService;
-
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(8);
     }
-
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                    .antMatchers("/", "/signIn").permitAll()
-                    .antMatchers("/user", "/car", "/carBrand", "/carModel", "typeAuto").hasRole("ADMIN")
-                    .antMatchers("/car").hasRole("USER")
-                    .anyRequest().authenticated()
+                .csrf().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                    .formLogin()
-                    .loginPage("/login")
-                    .defaultSuccessUrl("/")
-                    .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+//                .authorizeRequests()
+//                    .antMatchers("**/car/add", "**/car/delete/").hasAnyRole("USER", "ADMIN")
+//                    .antMatchers("**/delete/", "**/add/", "/api/car/getAll").hasRole("ADMIN")
+//                    .antMatchers("/login", "/registration").permitAll()
+//                    .antMatchers("/api/car/", "api/carBrand/", "api/carModel/", "api/typeAuto/").permitAll()
+//                    .antMatchers("/api/user/").hasRole("ADMIN")
+//                .and()
+                    .apply(new JwtConfigurer(jwtTokenProvider));
+    }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/swagger-ui/**", "/v3/api-docs/**");
     }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder());
+        auth.userDetailsService(userService).
+                passwordEncoder(bCryptPasswordEncoder());
     }
 
 }
