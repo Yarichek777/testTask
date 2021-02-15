30ab52f7f5792822ac8901ae28fd183e34fd95c2
Yaroslav Marchenko <yarik_marik_00@mail.ru>
2021-02-15 13:50:46 +0300

    create advice controller

---
+++ src/main/java/org/yarik/testTask/controller/AdviceController.java
@@ -0,0 +1,40 @@
+package org.yarik.testTask.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+
+import javax.validation.ConstraintViolationException;
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class AdviceController {
+
+    @ExceptionHandler(value = NoSuchElementException.class)
+    public ResponseEntity<String> noSuchElementException() {
+        return new ResponseEntity<String>("Not found element!", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = HttpClientErrorException.Forbidden.class)
+    public ResponseEntity<String> forbidden() {
+        return new ResponseEntity<>("Insufficient permissions to execute this request! Go to the login page", HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    public ResponseEntity<String> constraintViolationException() {
+        return new ResponseEntity<>("Not valid data in the request!", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResponseEntity<String> illegalArgumentException() {
+        return new ResponseEntity<>("Incorrect value in the request!", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = HttpClientErrorException.BadRequest.class)
+    public ResponseEntity<String> badRequest() {
+        return new ResponseEntity<>("Bad request, incorrect syntax in the request!", HttpStatus.BAD_REQUEST);
+    }
+
+}
